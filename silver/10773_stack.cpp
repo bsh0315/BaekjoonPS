bcde96f0b2e884cbb8cf986ffe714c502d2c902e
bsh0315 <shbackrok@gmail.com>
2025-04-09 11:58:50 +0900

    10773 추가 stack 자료구조 문제

---
+++ silver/10773_stack.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int K = 0 ;
+    cin >> K;
+    vector<int> num_arr(K);
+    vector<int> result_arr;
+    for(int i = 0; i<K ; ++i){
+        cin >> num_arr[i];
+        if(num_arr[i] == 0){
+            result_arr.pop_back();
+        }
+        else{
+            result_arr.push_back(num_arr[i]);
+        }
+    }
+
+    int sum = 0;
+    for(int i = 0; i<result_arr.size(); ++i){
+        sum += result_arr[i];
+    }
+
+    cout << sum << '\n';
+
+}
