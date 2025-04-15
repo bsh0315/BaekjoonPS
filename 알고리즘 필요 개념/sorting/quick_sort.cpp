d9f85505c1b11c487089da1e3e70b480d09f895c
bsh0315 <shbackrok@gmail.com>
2025-04-15 16:57:39 +0900

    퀴정렬 기본 및 최적화 버전 추가

--- 알고리즘 필요 개념/sorting/quick_sort.cpp
+++ 알고리즘 필요 개념/sorting/quick_sort.cpp
@@ -1,7 +1,51 @@
 // quick 정렬 기본 코드
+// 배열의 맨 마지막 원소를 pivot으로 설정했음.(오름차순)
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int partioning(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i = low -1;
     
+    for(int j = low; j<high; ++j){
+        if(pivot > arr[j]){ // 내림차순으로 하고싶은면 <로 바꾸기만 하면 됨.
+            ++i;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
+
+void quickSort(int arr[], int low, int high){
+    if(low < high){ // low < high 일 때까지.
+        int p = partioning(arr, low, high); 
+
+        quickSort(arr, low, p-1); 
+        // pivot보다 작은 부분들로 재귀적으로 실시.
+        quickSort(arr, p+1, high);
+        // pivot보다 큰 부분들로 재귀적으로 실시.
+    }
+    }
+
+ void printArray(int arr[], int size){
+    for(int i = 0; i<size; ++i){
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+ }
+
+
+int main(){
+    int arr[] = {8, 3, 1, 7, 0, 10, 2};
+    int size = sizeof(arr)/sizeof(int);
+
+    cout << "이전 배열" << '\n';
+    printArray(arr, size);
+
+    quickSort(arr, 0, size -1);
+
+    cout << "정렬 후 배열" << '\n';
+    printArray(arr, size);
 }
---
+++ 알고리즘 필요 개념/sorting/quick_sort_optimized.cpp
@@ -0,0 +1,75 @@
+// 퀵정렬 최적화 버전.
+// pivot 원소를 맨 뒤난 맨 앞의 것이 아닌. 중간의 원소를 선택함.
+// 이로써 정렬된 배열을 정렬하는 최악의 경우를 완화 가능.
+// 또한, 배열의 크기가 10 미만인 것은 삽입정렬을 통해서 최적화 가능.
+#include<bits/stdc++.h>
+using namespace std;
+
+int partitioning(int arr[], int low, int high){
+    int pivot = arr[(low+high)/2];
+    swap(arr[(low+high)/2], arr[high]);
+    int i = low -1;
+    for(int j = low; j<high; ++j){
+        if(pivot > arr[j]){
+            ++i;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[high], arr[i+1]);
+    return i+1;
+}
+
+void quickSort(int arr[], int low, int high){
+    if(low < high){
+        int p = partitioning(arr, low, high);
+
+        quickSort(arr, low, p-1);
+        quickSort(arr, p+1, high);
+    }
+    
+}
+
+// 삽입정렬
+void insertSort(int arr[], int size){
+    for(int i = 1; i<size; ++i){
+        int key = arr[i];
+        int j = i -1;
+        while(j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            --j; 
+        }
+        arr[j+1] = key; // 앞에서 --j에 의해 값이 감소했으므로 1 증가
+    }
+}
+
+void printArray(int arr[], int size){
+    for(int i = 0; i<size; ++i){
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+
+}
+
+
+int main() {
+    int arr[] = {8,3,1,7,0,10,2};
+    int size = sizeof(arr)/sizeof(int);
+
+    if(size <= 10){
+        cout << "정렬 전 배열" << '\n';
+        printArray(arr, size);
+        insertSort(arr, size);
+        cout << "삽입 정렬 후 배열" << '\n';
+        printArray(arr, size);
+        return 1;
+    }
+
+    cout << "이전 배열" << '\n';
+    printArray(arr, size);
+
+    quickSort(arr, 0, size-1);
+
+    cout << "정렬 후 배열" << '\n';
+    printArray(arr, size);
+
+}
