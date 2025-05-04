3d9622a32a2ab2d1bff09175ed362bfc438d924d
bsh0315 <shbackrok@gmail.com>
2025-05-04 23:09:43 +0900

    17219번 문제 추가

---
+++ silver/17219_map.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    map<string, string> map;
+    
+    for(int i = 0; i<n; ++i){
+        string key;
+        string value;
+        cin >> key >> value;
+        map[key] = value;
+    }
+
+    for(int i = 0; i<m; ++i){
+        string key;
+        cin >> key;
+        cout << map[key] << '\n';
+    }
+}
