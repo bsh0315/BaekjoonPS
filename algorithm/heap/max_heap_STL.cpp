f5e574546ed409fdb255afc07289328a47ac712b
bsh0315 <shbackrok@gmail.com>
2025-05-16 11:47:43 +0900

    algorithm/heap/max_heap* 부분 주석 수정

---
+++ algorithm/heap/max_heap_STL.cpp
@@ -0,0 +1,28 @@
+// 백준 11279 최대힙 문제 STL 이용버전.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tn;
+    cin >> tn;
+    priority_queue<int> pq;  // 최대 힙 (우선순위 큐)
+
+    while (tn--) {
+        int input;
+        cin >> input;
+        if (input == 0) {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+            } else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        } else {
+            pq.push(input);
+        }
+    }
+    return 0;
+}
