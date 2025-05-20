52bfc3bfba25225d48e5108557749aa1bd8bc7dc
bsh0315 <shbackrok@gmail.com>
2025-05-20 16:25:23 +0900

    15787 비트 마스킹 문제 추가

---
+++ silver/15787_bit_masking.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> train(n, 0);
+    vector<int> cnt(n, 0);
+    int order = 0;
+
+    for(int i = 0; i<m; ++i){
+        int order, n_tr, n_person;
+        cin >> order; 
+
+
+        switch(order){
+            case 1: 
+                cin >> n_tr >> n_person;
+                train[n_tr-1] |= (1 << n_person);
+                cnt[n_tr-1]++;
+                break;
+
+            case 2:
+                cin >> n_tr >> n_person;
+                train[n_tr-1] &= ~(1 << n_person);
+                break;
+
+            case 3:
+                cin >> n_tr;
+                train[n_tr-1] = (train[n_tr-1] << 1) & (1 << 21 - 2);
+                break;
+
+            case 4:
+                cin >> n_tr;
+                train[n_tr-1] >>=1;
+                break;
+            
+            default: 
+                break;
+        }
+    }
+
+    unordered_set<int> s(train.begin(), train.end());
+    cout << s.size() << '\n';
+    
+}
