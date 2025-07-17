7c6bc4876c1a3d7722f814ba0e72287382d854f0
bsh0315 <shbackrok@gmail.com>
2025-07-17 15:58:51 +0900

    3079 매개변수 탐색 문제 추가

---
+++ gold/3079_BS.cpp
@@ -0,0 +1,18 @@
+// 매개변수 탐색 이용.
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n, m;
+    cin >> n >> m;
+    vector<long long> times(n);
+
+    for(int i = 0; i<n; ++i){
+        cin >> times[i];
+    }
+
+
+}
